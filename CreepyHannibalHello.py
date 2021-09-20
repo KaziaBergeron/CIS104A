2428dd6023f1b38dc6343ea74a0d8b3624d9d276
KaziaBergeron <90808661+KaziaBergeron@users.noreply.github.com>
2021-09-19 20:36:11 -0700

    Fixed bracket, line 2

--- CreepyHannibalHello.py
+++ CreepyHannibalHello.py
@@ -1,5 +1,5 @@
-print('Hello, Clarice');
-print('Kazia Bergeron'}
+print('Hello, Clarice')
+print('Kazia Bergeron')
 print('September 19, 2021')
 print('At what size of a project should you start using version management software? Why?')
 print('I think version management software is useful for any size project. The software keeps a detailed log of changes made to the file, so long as everyone who is making those changes are leaving decriptive commits. This makes it much easier to go back to previous versions to correct any changes that were made to the file and ended up causing issues or problems within the program. This is even useful for a project that you are working on by yourself, especially if the project is long and involves weeks or months or maybe even years of work. Having that log of changes made and why they were made can save you a lot of headache later on. So, to answer the question, it is in my opinion that version management software should be used for any project. However, if you are working on a project with 2 or more people, you will definitely want to use version management software.') 
