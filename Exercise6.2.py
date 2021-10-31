035db0508769c267ec6e47d8f67d03665f7ca248
KaziaBergeron <90808661+KaziaBergeron@users.noreply.github.com>
2021-10-31 12:38:05 -0400

    Uploaded Module 6 files

---
+++ Exercise6.2.py
@@ -0,0 +1,7 @@
+fruit = 'banana'
+print(fruit[:])
+
+#fruit[:] is the whole string stored in the variable 'fruit'
+#this will print out the whole word 'banana'
+#there is no information stored on either side of the :, so the string begins
+#at the beginning and ends at the end.
